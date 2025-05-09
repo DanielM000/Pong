5cc20d6d0ffc1965c359bc31a5da78b08ec19578
DanielM000 <daniel.mohammadi@elev.ga.ntig.se>
2025-05-09 15:07:01 +0200

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,11 +1,5 @@
 from turtle import Turtle, Screen
 
-ax = 350
-ay = 0
-
-bx = -350
-by = 0
-
 screen = Screen()
 screen.setup(800, 600)
 screen.title("Pong")
@@ -18,7 +12,7 @@ spelfigur1.shape("square")
 spelfigur1.shapesize(6, 1)
 spelfigur1.color("white")
 spelfigur1.penup()
-spelfigur1.setposition(ax, ay)
+spelfigur1.setposition(350, 0)
 def spelfigur1_up():
     ny_plats1 = spelfigur1.ycor() + 20
     spelfigur1.goto(spelfigur1.xcor(), ny_plats1)
@@ -32,7 +26,7 @@ spelfigur2.shape("square")
 spelfigur2.shapesize(6, 1)
 spelfigur2.color("white")
 spelfigur2.penup()
-spelfigur2.setposition(bx, by)
+spelfigur2.setposition(-350, 0)
 def spelfigur2_up():
     ny_plats2 = spelfigur2.ycor() + 20
     spelfigur2.goto(spelfigur2.xcor(), ny_plats2)
@@ -42,14 +36,25 @@ def spelfigur2_down():
     spelfigur2.goto(spelfigur2.xcor(), ny_plats2)
 
 bollen = Turtle()
-bollen.shape(“circle”)
-bollen.color(“white”)
-
+bollen.shape("circle")
+bollen.color("white")
+bollen.penup()
+def flytta_bollen():
+	boll_x = bollen.xcor() + 10
+	boll_y = bollen.ycor() + 10
+	bollen.goto(boll_x, boll_y)
+    
 
 screen.listen()
 screen.onkey(spelfigur1_up, "Up")
 screen.onkey(spelfigur1_down, "Down")
 screen.onkey(spelfigur2_up, "w")
 screen.onkey(spelfigur2_down, "s")
+
+game_is_on = True
+while game_is_on == True:
+    flytta_bollen()
+    if boll_y == 300:
+        boll_y = bollen.ycor - 10
 
 screen.exitonclick()
